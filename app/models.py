408d92b023ee8e025443306f6e783b0d77857e48
imguagua <skyfan1981@gmail.com>
2016-04-21 08:57:50 +0800

    update

--- app/models.py
+++ app/models.py
@@ -3,10 +3,18 @@ from flask import current_app
 from . import db
 
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    
+
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    email = db.Colum(db.String(64), unique=True)
+
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
